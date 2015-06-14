0b80469711c71e73c8bb2a79cfe5d30904a39734
Jinaraj <jinaraj@Jinarajs-MacBook-Pro.local>
2015-06-14 16:17:26 +0530

    Initial Commit

---
+++ NxtStepZ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NxtStepZ
+//
+//  Created by Jinaraj on 6/14/15.
+//  Copyright (c) 2015 Jinaraj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NxtStepZ/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  NxtStepZ
+//
+//  Created by Jinaraj on 6/14/15.
+//  Copyright (c) 2015 Jinaraj. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
